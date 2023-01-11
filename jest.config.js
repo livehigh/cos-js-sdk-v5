9f2cc4329ceb4a3cdc0bf436fb7a08fbaab9b99d
chrisftian <chrisftian@tencent.com>
2023-01-11 10:50:40 +0800

    fix: 修改jest配置

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverage: true,  // 是否显示覆盖率报告
   // 告诉 jest 哪些文件需要经过单元测试测试
-  collectCoverageFrom: ['test/test.js'],
+  collectCoverageFrom: ['**/src/advance.js', '**/src/base.js'],
   coverageThreshold: {
     global: {
       statements: 90, // 保证每个语句都执行了
